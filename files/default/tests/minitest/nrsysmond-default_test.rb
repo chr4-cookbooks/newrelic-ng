13fdecfc4146b5a758d9b5f539d68926c405886b
Chris Aumann <me@chr4.org>
2013-06-21 18:05:14 +0200

    add tests

---
+++ files/default/tests/minitest/nrsysmond-default_test.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: newrelic-ng
+# Test:: nrsysmond-default
+#
+# Copyright 2012, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'newrelic-ng::nrsysmond-default' do
+  include Helpers::TestHelper
+
+  it 'installs newrelic-sysmond package' do
+    package('newrelic-sysmond').must_be_installed
+  end
+
+  it 'must set license key ' do
+    file(node['newrelic-ng']['nrsysmond']['config_file']).must_include('TESTKEY_NRSYSMOND')
+  end
+
+  it 'service newrelic-sysmond must be enabled' do
+    # service status doesn't work, so grepping process table instead
+    cmd = shell_out('ps aux |grep -v grep |grep -q rsysmond')
+    cmd.exitstatus.to_s.must_include('0')
+
+    service('newrelic-sysmond').must_be_enabled
+  end
+end

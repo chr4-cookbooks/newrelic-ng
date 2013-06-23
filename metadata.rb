6449a89bf53ebad6a4f9957fb5787cbde78f4e9f
Chris Aumann <me@chr4.org>
2013-06-23 10:29:58 +0200

    add supported platforms to metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'apt'
 depends          'python'
+
+%w{ ubuntu debian redhat centos fedora amazon suse scientific }.each do |os|
+  supports os
+end

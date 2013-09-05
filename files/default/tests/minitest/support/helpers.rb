3e189fe1a17454eff1b5ffc1b420ad7421f7babe
Cameron Johnston <cameron@needle.com>
2013-09-05 17:51:06 -0600

    add a which method to test helpers and use it for locating newrelic_plugin_agent executable

--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -24,5 +24,16 @@ module Helpers
     include MiniTest::Chef::Context
     include MiniTest::Chef::Resources
 
+    # courtesy http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      return nil
+    end
   end
 end

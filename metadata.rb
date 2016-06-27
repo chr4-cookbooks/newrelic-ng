094037b95f975f6dba698f7a733ed998756e8ae8
Ofir Petrushka <opetrushka@zendesk.com>
2016-06-27 11:11:01 +1000

    make zendesk

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
-name             'newrelic-ng'
+# This was only made zendesk for progressing until community PR will get accepted:
+# https://github.com/chr4-cookbooks/newrelic-ng/issues/33
+name             'zendesk_newrelic-ng'
 maintainer       'Chris Aumann'
 maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures newrelic monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.4'
+version          '0.5.5'
 depends          'apt'
 depends          'build-essential'
 depends          'python'

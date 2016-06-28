b191d37f64c90a0bae8d2e7e9e0def181d0dc974
Ofir Petrushka <opetrushka@zendesk.com>
2016-06-28 13:47:15 +1000

    add metadata required by foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures newrelic monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url       'https://github.com/chr4-cookbooks/newrelic-ng/issues'
-source_url       'https://github.com/chr4-cookbooks/newrelic-ng'
+issues_url       'https://github.com/chr4-cookbooks/newrelic-ng/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/chr4-cookbooks/newrelic-ng' if respond_to?(:source_url)
 version          '0.6.0'
 depends          'apt'
 depends          'build-essential'
